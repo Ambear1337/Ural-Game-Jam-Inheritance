58cc0f3226a909cbe3cc19ee32b5142006b98dc0
Artyom Bakhterev <delta38tema@gmail.com>
2026-03-20 01:22:07 +0500

    addition to previous commit

---
+++ Assets/Scripts/ProjectGame/Creatures/CreaturesPool.cs
@@ -0,0 +1,56 @@
+using Sisus.Init;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace ProjectGame
+{
+    [Service(AddressableKey = "CharactersPool", Instantiate = true)]
+    public class CreaturesPool: MonoBehaviour
+    {
+        // The pool holds plain GameObjects (you can swap this for any component type).
+        public ObjectPool<Creature> Pool;
+
+        [SerializeField] private Creature _creature;
+
+        private void Awake()
+        {
+            // Create a pool with the four core callbacks.
+            Pool = new ObjectPool<Creature>(
+                createFunc: CreateItem,
+                actionOnGet: OnGet,
+                actionOnRelease: OnRelease,
+                actionOnDestroy: OnDestroyItem,
+                collectionCheck: true,   // helps catch double-release mistakes
+                defaultCapacity: 3,
+                maxSize: 5
+            );
+        }
+
+        // Creates a new pooled GameObject the first time (and whenever the pool needs more).
+        private Creature CreateItem()
+        {
+            var creature = Instantiate(_creature);
+            creature.name = "Character";
+            creature.gameObject.SetActive(false);
+            return creature;
+        }
+
+        // Called when an item is taken from the pool.
+        private void OnGet(Creature creature)
+        {
+            creature.gameObject.SetActive(true);
+        }
+
+        // Called when an item is returned to the pool.
+        private void OnRelease(Creature creature)
+        {
+            creature.gameObject.SetActive(false);
+        }
+
+        // Called when the pool decides to destroy an item (e.g., above max size).
+        private void OnDestroyItem(Creature creature)
+        {
+            Destroy(creature.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectGame/UI/CreatureUI.cs
@@ -0,0 +1,13 @@
+using ProjectGame.UI;
+using UnityEngine;
+
+public class CreatureUI: MonoBehaviour, ISelectable
+{
+    [SerializeField]
+    private Sprite _creatureSprite;
+
+    private void Select()
+    {
+        
+    }
+}
