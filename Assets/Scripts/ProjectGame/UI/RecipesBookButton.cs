77de568406010fa672f519c10870cbb298ae1a45
Artyom Bakhterev <delta38tema@gmail.com>
2026-03-28 22:26:41 +0500

    Added some files (addition to previous commit)

---
+++ Assets/Scripts/ProjectGame/UI/RecipesBookButton.cs
@@ -0,0 +1,12 @@
+using ProjectGame.Buttons;
+using UnityEngine;
+
+public class RecipesBookButton: MenuButtonLeftClickBase
+{
+    [SerializeField] private GameObject _recipesPanel;
+    
+    protected override void FireEvent()
+    {
+        
+    }
+}
